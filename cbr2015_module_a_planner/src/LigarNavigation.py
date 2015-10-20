3a227ecf0e907dcb5af016915b865e31630ca127
Heverton29 <ms.heverton@hotmail.com>
2015-10-20 00:01:36 -0200

    cbr2015 Modulo A e B

--- cbr2015_module_a_planner/src/LigarNavigation.py
+++ cbr2015_module_a_planner/src/LigarNavigation.py
@@ -5,18 +5,14 @@ from geometry_msgs.msg import Point
 from move_base_msgs.msg import MoveBaseGoal
 from move_base_msgs.msg import MoveBaseAction
 from geometry_msgs.msg import Quaternion
+from robotino_motion.msg import AlignAction
+from robotino_motion.msg import AlignGoal
 from tf import transformations
 import tf
 import actionlib
 from actionlib import SimpleActionClient
 
-
-'''def ligarNavigation(area, seq):
-	rospy.logwarn("Indo para pose "+ str(area[1]) + str(area[2]))
-	time.sleep(30)
-	rospy.logwarn("Cheguei no pose "+ str(area[1]) + str(area[2]))
-'''
-def ligarNavigation(area, seq):
+def ligarNavigation(area, seq, nome):
 	#send_goal
 	client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
 
@@ -25,23 +21,21 @@ def ligarNavigation(area, seq):
 
 	goal = MoveBaseGoal()
 
-	q = transformations.quaternion_from_euler(0, 0, area[3])
+	q = transformations.quaternion_from_euler(0, 0, area[2])
 	quat = Quaternion(*q)
 
 	goal.target_pose.header.seq = seq
 	goal.target_pose.header.frame_id = "map"
         goal.target_pose.header.stamp = rospy.Time.now()
 
-	goal.target_pose.pose.position.x = area[1]
-	goal.target_pose.pose.position.y = area[2]
+	goal.target_pose.pose.position.x = area[0]
+	goal.target_pose.pose.position.y = area[1]
 	goal.target_pose.pose.orientation = quat
 
 	#pub = rospy.Publisher("move_base_simple/goal", PoseStamped)
 	#pub.publish(goal.target_pose)
 
-	print "antes do wait"
         client.wait_for_server()
-	print "depois do wait"
 
         # Sends the goal to the action server.
         client.send_goal(goal)
@@ -49,7 +43,55 @@ def ligarNavigation(area, seq):
         # Waits for the server to finish performing the action.
         client.wait_for_result()
 
-	rospy.logwarn("Cheguei no pose "+ str(area[1]) +" "+ str(area[2]))
+	rospy.logwarn("Cheguei no pose "+ str(area[0]) +" "+ str(area[1]) +"Area = "+nome)
+
+	if nome == "Pedidos":
+		rospy.logwarn("Vou alinhar a direita")
+		client = actionlib.SimpleActionClient('align', AlignAction)
+		client.wait_for_server()
+
+		goal = AlignGoal()
+
+		goal.alignment_mode = 1
+		goal.distance_mode = 1
+
+		# Sends the goal to the action server.
+		client.send_goal(goal)
+
+		# Waits for the server to finish performing the action.
+		client.wait_for_result()
+		
+	if nome == "Casa":
+		rospy.logwarn("Vou alinhar a direita")
+		client = actionlib.SimpleActionClient('align', AlignAction)
+		client.wait_for_server()
+
+		goal = AlignGoal()
+
+		goal.alignment_mode = 1
+		goal.distance_mode = 1
+
+		# Sends the goal to the action server.
+		client.send_goal(goal)
+
+		# Waits for the server to finish performing the action.
+		client.wait_for_result()
+	
+		rospy.logwarn("Vou alinhar atras")
+		client = actionlib.SimpleActionClient('align', AlignAction)
+		client.wait_for_server()
+
+		goal.alignment_mode = 3
+		goal.distance_mode = 1
+
+		# Sends the goal to the action server.
+		client.send_goal(goal)
+
+		# Waits for the server to finish performing the action.
+		client.wait_for_result()
+
+	
+
 	'''if client.getState() == actionlib.SimpleClientGoalState.SUCCEEDED:
 		ROS_INFO("Cheguei no pose "+ str(area[0]) + str(area[1]));
 	else:
--- cbr2015_module_b_planner/src/sm_org.py
+++ cbr2015_module_b_planner/src/sm_org.py
@@ -8,20 +8,28 @@ from enum import *
 from enum import areaOrganizada
 global buffer_empty
 global success
-global prat_des
-global prat_aux
-global obj_des
-global obj_aux
 global objeto
 global terminou
+global prox_area_aux
+global prox_area
+global area_verifica
+global areas_ogz
+global cores
+cores = [1, 2, 3, 4]
+areas = [Areas.A1, Areas.A2, Areas.A3, Areas.A4, Areas.B1, Areas.B2, Areas.B3, Areas.B4, Areas.CASA]
+areas_ogz = [AreasOrganizadas.A1, AreasOrganizadas.A2, AreasOrganizadas.A3, AreasOrganizadas.A4, AreasOrganizadas.B1, AreasOrganizadas.B2, AreasOrganizadas.B3, AreasOrganizadas.B4]
 from pegando_objeto import *
 from deixando_objeto import *
 from indo_para_area import *
+from ligando_leds import *
 
 buffer_empty = True
 success = False
 terminou = False
 objeto = Objetos.NONE
+prox_area_aux = []
+prox_area = []
+area_verifica = AreasOrganizadas.A1
 
 seq = 0
 
@@ -33,34 +41,61 @@ class IndoParaArea(smach.State):
 
     def execute(self, userdata):
 	global seq
+	global cores
 	seq += 1
-	#indoParaArea(userdata.area, seq)
+	if userdata.area[0] == Areas.CASA:
+		ligandoLeds(cores)
+		indoParaArea(userdata.area, seq)
+		desligandoLeds()
+		return 'chegou'
+	indoParaArea(userdata.area, seq)
 	rospy.logwarn('Cheguei')
 	return 'chegou'
 
 # define state EstouNaArea
 class EstouNaArea(smach.State):   
     def __init__(self):
-        smach.State.__init__(self, outcomes=['pegar_obj', 'deixar_obj', 'fim_org'],
+        smach.State.__init__(self, outcomes=['pegar_obj', 'deixar_obj', 'fim_org', 'comeca_org'],
 				input_keys=['area_atual', 'area_des', 'area_aux'],
-				output_keys=['prox_area', 'area_parc'])
+				output_keys=['prox_area', 'area_parc', 'nova_area_des', 'nova_area_aux'])
 
     def execute(self, userdata):
 	global objeto
 	global terminou
+	global areas
+	global prox_area_aux
+	global prox_area
 	rospy.logwarn("Area Atual: %s, Area Desejada: %s, Area Auxiliar: %s", userdata.area_atual, userdata.area_des, userdata.area_aux)
-	if userdata.area_atual[0] == Areas.CASA[0] and terminou:
-		rospy.logwarn('Finalizei e estou em casa')
-		return 'fim_org'
+	if userdata.area_atual[0] == Areas.CASA[0] and not terminou:
+		prox_area = areas[0]		
+		userdata.prox_area = prox_area
+		userdata.nova_area_des = prox_area
+		novaAreaAux(prox_area)
+		userdata.nova_area_aux = prox_area_aux
+		rospy.logwarn('Estou em casa e comecarei a organizar, Area Des: %s, Area Aux:%s', prox_area, prox_area_aux)
+		return 'comeca_org'
+	if userdata.area_atual[0] == Areas.CASA[0] and terminou:		
+		if areas[0] == Areas.CASA:
+			rospy.logwarn('Finalizei e estou em casa')
+			rospy.logwarn('Como ficaram Areas A: %s, %s, %s, %s', Areas.A1, Areas.A2, Areas.A3, Areas.A4)
+			rospy.logwarn('Como ficaram Areas B: %s, %s, %s, %s', Areas.B1, Areas.B2, Areas.B3, Areas.B4)
+			return 'fim_org'
+		prox_area = areas.pop(0)
+		userdata.prox_area = prox_area
+		userdata.nova_area_des = prox_area
+		novaAreaAux(prox_area)
+		userdata.nova_area_aux = prox_area_aux
+		rospy.logwarn('Acabei de organizar mais um')
+		terminou = False
+		return 'comeca_org'
 	if userdata.area_atual[0] == userdata.area_des[0]:
-		rospy.logwarn('Estou na Area Desejada')
+		rospy.logwarn('Estou na Area Desejada com o objeto %s, buffer %s', userdata.area_atual[4], buffer_empty)
 		if areaOrganizada(userdata.area_atual, objeto) and buffer_empty:			
 			rospy.logwarn('Estou na Prateleira Desejada com Objeto DESEJADO na primeira passada')
 			success = True
-			terminou = True
-			userdata.area_parc = userdata.area_atual			
+			terminou = True			
 			userdata.prox_area = Areas.CASA
-			return 'deixar_obj'
+			return 'comeca_org'
 		if not areaOrganizada(userdata.area_atual, objeto) and buffer_empty:
 			rospy.logwarn('Estou na Prateleira Desejada com Objeto AUXILIAR')
 			userdata.area_parc = userdata.area_atual			
@@ -115,7 +150,7 @@ class PegandoObjeto(smach.State):
 	global objeto
 	rospy.logwarn('Peguei e vou para a proxima area')
 	objeto = userdata.area_atual[4]
-	pegandoObjeto(userdata.area_atual)
+	pegandoObjeto(userdata.area_atual, objeto[1])
 	rospy.logwarn("%s", userdata.area_atual)
 	if userdata.area_atual[0] == Areas.BUFFER[0]:
 		buffer_empty = True
@@ -131,9 +166,58 @@ class DeixandoObjeto(smach.State):
 	global buffer_empty
 	global objeto
 	deixandoObjeto(userdata.area_atual, objeto)
+	rospy.logwarn('Deixei :%s na Area:%s', objeto, userdata.area_atual)
+	objeto = Objetos.NONE
 	if userdata.area_atual[0] == userdata.area_aux[0]:
 		rospy.logwarn('Deixei e vou voltar para casa')
 		return 'deixou'
 	buffer_empty = False
 	rospy.logwarn('Deixei e vou para a proxima area')
 	return 'deixou'
+
+def novaAreaAux(area_des):
+	global prox_area_aux
+	global areas_ogz
+	global area_verifica
+	atualizaAreasOgz()
+	for  i in range (0, 6):
+		i += 1
+		if area_des[0] == area_verifica[0]:
+			if area_verifica[1] == Areas.A1[4]:
+				if Areas.A1[4] != AreasOrganizadas.A1[1]:
+					prox_area_aux = Areas.A1
+					return
+			elif area_verifica[1] == Areas.A2[4]:
+				if Areas.A2[4] != AreasOrganizadas.A2[1]:
+					prox_area_aux = Areas.A2		
+					return	
+			elif area_verifica[1] == Areas.A3[4]:
+				if Areas.A3[4] != AreasOrganizadas.A3[1]:
+					prox_area_aux = Areas.A3		
+					return	
+			elif area_verifica[1] == Areas.A4[4]:
+				if Areas.A4[4] != AreasOrganizadas.A4[1]:
+					prox_area_aux = Areas.A4		
+					return	
+			elif area_verifica[1] == Areas.B1[4]:
+				if Areas.B1[4] != AreasOrganizadas.B1[1]:
+					prox_area_aux = Areas.B1		
+					return	
+			elif area_verifica[1] == Areas.B2[4]:
+				if Areas.B2[4] != AreasOrganizadas.B2[1]:
+					prox_area_aux = Areas.B2		
+					return	
+			elif area_verifica[1] == Areas.B3[4]:
+				if Areas.B3[4] != AreasOrganizadas.B3[1]:
+					prox_area_aux = Areas.B3		
+					return	
+			elif area_verifica[1] == Areas.B4[4]:
+				if Areas.B4[4] != AreasOrganizadas.B4[1]:
+					prox_area_aux = Areas.B4		
+					return
+		area_verifica = areas_ogz.pop(0)
+	area_verifica = AreasOrganizadas.A1
+
+def atualizaAreasOgz():
+	global areas_ogz
+	areas_ogz = [AreasOrganizadas.A1, AreasOrganizadas.A2, AreasOrganizadas.A3, AreasOrganizadas.A4, AreasOrganizadas.B1, AreasOrganizadas.B2, AreasOrganizadas.B3, AreasOrganizadas.B4]
