1dff20495c0d78ccd99f93253133553761590bdf
Heverton29 <ms.heverton@hotmail.com>
2015-10-23 21:43:52 -0200

    Modulo B pegando, deixando e lendo

--- cbr2015_module_b_planner/src/deixando_objeto.py
+++ cbr2015_module_b_planner/src/deixando_objeto.py
@@ -1,8 +1,23 @@
 import rospy
 from enum import *
+from std_msgs.msg import String
+from robotino_leds.srv import *
+from robotino_motion.msg import StorePuckAction
+from robotino_motion.msg import StorePuckGoal
+import actionlib
+from actionlib import SimpleActionClient
 
 def deixandoObjeto(area, objeto):
-	'''
-	aqui vai entrar o deixar puck do Au
-	'''
-	atualizaArea(area, objeto)	
+	client = actionlib.SimpleActionClient('store_puck', StorePuckAction)
+        client.wait_for_server()
+
+	goal = StorePuckGoal()
+	goal.mode = 0
+
+        client.send_goal(goal)
+
+        client.wait_for_result()
+
+	rospy.logwarn("Entregando Produto")
+
+	atualizaArea(area, objeto)
